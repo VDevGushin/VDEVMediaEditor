10f0f1dff833f1eff58a30a28083d680e4f521c1
Vladislav Gushchin <v.d.gushin@live.ru>
2023-05-05 17:48:53 +0300

    Fix aspect ratio button

--- Sources/VDEVMediaEditor/MediaEditor/MediaEditor/Presentation/CanvasControls/ToolsArea/ToolsLayers/LayersView.swift
+++ Sources/VDEVMediaEditor/MediaEditor/MediaEditor/Presentation/CanvasControls/ToolsArea/ToolsLayers/LayersView.swift
@@ -11,6 +11,7 @@ struct LayersView: View {
     @EnvironmentObject private var vm: CanvasEditorViewModel
     @Injected private var images: VDEVImageConfig
     @Injected private var strings: VDEVMediaEditorStrings
+    @Injected private var settings: VDEVMediaEditorSettings
     
     var body: some View {
         Group {
@@ -78,15 +79,17 @@ struct LayersView: View {
                             Log.d("Select tool backgroundColor")
                         }.scaleEffect(0.95)
                         
-                        //Aspect ratio
-                        NoShadowRoundButtonSystem(image: Image(systemName: "arrow.down.forward.and.arrow.up.backward"),
-                                            size: 40,
-                                            backgroundColor: AppColors.layerButtonsLightBlack,
-                                            tintColor: AppColors.white) {
-                            vm.tools.openLayersList(false)
-                            vm.tools.seletedTool(.aspectRatio)
-                            Log.d("Select tool aspect ratio")
-                        }.scaleEffect(0.95)
+                        if !settings.isInternalModule {
+                            //Aspect ratio
+                            NoShadowRoundButtonSystem(image: Image(systemName: "arrow.down.forward.and.arrow.up.backward"),
+                                                      size: 40,
+                                                      backgroundColor: AppColors.layerButtonsLightBlack,
+                                                      tintColor: AppColors.white) {
+                                vm.tools.openLayersList(false)
+                                vm.tools.seletedTool(.aspectRatio)
+                                Log.d("Select tool aspect ratio")
+                            }.scaleEffect(0.95)
+                        }
                     }
                     .padding(.vertical, 15)
                     .frame(maxWidth: .infinity)
